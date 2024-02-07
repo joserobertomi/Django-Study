a360bf5921bb748996fd1b57ad6bcc425d1f08bc
jrcodee <joserobertomi1203@gmail.com>
2024-02-07 12:05:07 -0300

    data injection in templates

--- project1/culinary_recipes/views.py
+++ project1/culinary_recipes/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
+from utils.culinary_recipes.factorys import make_recipe
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'culinary_recipes/pages/home.html')
+    return render(request, 'culinary_recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(12)]})
 
 
 def recipe(request, id):
-    return render(request, 'culinary_recipes/pages/recipe_card.html')
+    return render(request, 'culinary_recipes/pages/recipe_card.html', context={'recipe': make_recipe()})
