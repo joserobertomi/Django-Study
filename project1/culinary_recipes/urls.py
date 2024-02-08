39e22dcf62fe45ce134a23343ebf15811d042133
jrcodee <joserobertomi1203@gmail.com>
2024-02-08 15:06:13 -0300

    add dynamic uls with namespace

--- project1/culinary_recipes/urls.py
+++ project1/culinary_recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'culinary_recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipe/<id>/', views.recipe)
+    path('', views.home, name="home"),
+    path('recipe/<id>/', views.recipe, name="card")
 ]
