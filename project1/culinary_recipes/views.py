dd2dc9f5e680556364ce7a453462929c5e893dd9
jrcodee <joserobertomi1203@gmail.com>
2024-02-09 12:33:38 -0300

    change category title and raise 404 when the category is None

--- project1/culinary_recipes/views.py
+++ project1/culinary_recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.culinary_recipes.factorys import make_recipe
 from culinary_recipes.models import Recipe
+from django.http import Http404
 
 # Create your views here.
 
@@ -11,9 +12,15 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'culinary_recipes/pages/categorys.html', context={'recipes': recipes})
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404("Page Not Found 😢")
+
+    return render(request, 'culinary_recipes/pages/categorys.html', context={
+        'recipes': recipes,
+        'category_title': f'{recipes.first().category.name}'
+    })
 
 
 def recipe(request, id):
