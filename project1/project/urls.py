8f47cac479d3ab090543e5839193f44d0fde4094
jrcodee <joserobertomi1203@gmail.com>
2024-02-23 23:34:14 -0300

    updating the url

--- project1/project/urls.py
+++ project1/project/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('culinary-recipes/', include('culinary_recipes.urls')),
+    path('', include('culinary_recipes.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
