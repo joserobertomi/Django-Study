26669abebad5d8e0581acca9acca92e72fd1ae5d
jrcodee <joserobertomi1203@gmail.com>
2024-02-24 00:03:52 -0300

    including first tests

--- project1/culinary_recipes/tests.py
+++ project1/culinary_recipes/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestingSetUp(TestCase):
+
+    def test_is_set_up_ok(self):
+        assert 1 == 1, "OK"
+
+
+class RecipeURLsTest(TestCase):
+
+    def test_culinary_recipes_home_url_is_correct(self):
+        home_url = reverse('culinary_recipes:home')
+        self.assertEqual(home_url, '/')
