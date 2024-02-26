5ed0b68fbb475074bef0b8ba2528f16796f731bc
jrcodee <joserobertomi1203@gmail.com>
2024-02-26 19:09:29 -0300

    add more home tests

--- project1/tests/test_recipe_view.py
+++ project1/tests/test_recipe_view.py
@@ -5,8 +5,16 @@ from culinary_recipes.views import home, category, recipe
 
 class RecipeViewTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
-        view = resolve('/')
+        view = resolve(reverse('culinary_recipes:home'))
         self.assertIs(view.func, home)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('culinary_recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('culinary_recipes:home'))
+        self.assertTemplateUsed(response, 'culinary_recipes/pages/home.html')
 
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(reverse('culinary_recipes:category', kwargs={'category_id': 1}))
